366a68bb59d7a17d9c34c353df6e2f4824a827b5
Sidney Crandall <sdncrandall5@gmail.com>
2021-05-24 11:50:14 -0500

    Enemies List complete

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,55 @@
 // Put your code here
+
+console.log( "My Enemies: ")
+console.log("----------------")
+
+// creating a variable that will hold the object and their values for the function to list enemies
+const enemies = [
+    {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        offensesL: [
+            "Being a jerk to me in elementary school",
+            "Not being nice to me in elementary school"
+        ],
+        isReallyHated: true
+    },
+    {
+        firstName: "Darth",
+        lastName: "Vader",
+        offensesL: [
+            "Cut off Luke's hand",
+            "Murdered all those kids",
+            "Unkind management practices"
+        ],
+        isReallyHated: false
+    },
+    {
+        firstName: "Betty",
+        lastName: "Rudelady",
+        offensesL: [
+            "Phone calls in the theater",
+            "Phone calls on the bus",
+            "Phone calls in line at the grocery store",
+            "Poor conversationalist"
+        ],
+        isReallyHated: true
+    },
+    {
+        firstName: "Leon",
+        lastName: "Peck",
+        offensesL: [
+            "Keeps giving me a hotplate"
+        ],
+        isReallyHated: false
+    }
+]
+
+// iterate through the array and console log the most hated with a comment
+for ( const enemy of enemies ) {
+    if (enemy.isReallyHated === true) {
+        console.log(`${enemy.firstName} ${enemy.lastName} (Never forget!)`);
+    } else {
+        console.log(`${enemy.firstName} ${enemy.lastName}`)
+    }
+}
